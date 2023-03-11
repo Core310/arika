de661358c02616f01047705aacffbd4d9dec90aa
Something Something <arika.khor@gmail.com>
2023-03-11 15:02:19 +0800

    add particles

---
+++ particles/app.js
@@ -0,0 +1,9 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+
+particlesJS.load('particles-js', 'particles/particles.json', function () {
+  console.log('particles.js loaded - callback');
+});
+
